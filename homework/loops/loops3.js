d148f0771e2f403a4bce7529a1085845762a30b2
설설이 <leesugar90@gmail.com>
2020-07-07 23:35:55 +0900

    별찍기 3번째 과제 수정

--- homework/loops/loops3.js
+++ homework/loops/loops3.js
@@ -1,22 +1,40 @@
-String.prototype.replaceAt = function(start, end, replacement) {
-  return (
-    this.substring(0, start) +
-    replacement +
-    this.substring(end + 1, this.length)
-  );
-};
+// String.prototype.replaceAt = function(start, end, replacement) {
+//   return (
+//     this.substring(0, start) +
+//     replacement +
+//     this.substring(end, this.length - 1)
+//   );
+// };
 
-let star = "**********";
-for (let i = 0; i < 5; i++) {
-  let start = i + 1;
-  let end = star.length - (i + 2);
-  let replacement = " ";
-  if (start < end) {
-    for (let j = start; j < end; j++) {
-      replacement += " ";
-    }
-  } else {
-    replacement = "";
+// let star = "*********";
+// for (let i = 0; i < 5; i++) {
+//   let start = i + 1;
+//   let end = star.length - (i + 1);
+//   let replacement = " ";
+//   if (start < end) {
+//     for (let j = start; j < end; j++) {
+//       replacement += " ";
+//     }
+//   } else {
+//     replacement = "";
+//   }
+
+//   console.log(star.replaceAt(start, end, replacement));
+// }
+
+// lesson
+const n = 5;
+const w = 2 * n - 1;
+// const mid = Math.round(w / 2);
+// console.log(mid);
+
+for (let y = 0; y < n; y++) {
+  let string = "";
+  // const blank = 2 * n - 1 - 2 * y;
+  for (let x = 0; x < w; x++) {
+    // if(x>mid)
+    if (x > y && x < w - (y + 1)) string += " ";
+    else string += "*";
   }
-  console.log(star.replaceAt(start, end, replacement));
+  console.log(string);
 }
